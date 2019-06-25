7d3466c11403e957cdaf0ae1a42be77d9eee5c17
github username <github email address>
2019-06-25 19:23:47 -0400

    NOT DONE

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,36 @@ require 'pry'
 
 class String
 
+  # returns true if the string that you are calling this method on ends in a period.
+  # returns false if the string that you are calling this method on does NOT end in a period.
   def sentence?
-    
+    self.end_with?(".")
   end
 
+  # returns true if the string that you are calling this method on ends in a question mark.
+  # returns false if the string that you are calling this method on does NOT end in question mark.
   def question?
-
+    self.end_with?("?")
   end
 
+  # returns true if the string that you are calling this method on ends in an exclamation mark
+  # returns false if the string that you are calling this method on does NOT end in a exclamation mark.
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  # returns the number of sentences in a string
+  # returns zero if there are no sentences in a string
+  # returns the number of sentences in a complex string
   def count_sentences
+    sentences_count = []
+    if self.class == Array
+      self.each do |sentence|
+        
+      end
+    end
+    sentences_count = self.split(".")
+    sentences_count.length
 
   end
 end
